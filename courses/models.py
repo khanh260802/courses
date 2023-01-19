b8b5dd95b777a84e8d4b808509d9b7bafb43c25a
khanh260802 <casiofx580@gmail.com>
2023-01-19 23:40:30 +0700

    learn how to create project, connect database

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='uploads/%Y/%m')
+class Category(models.Model): 
+    name = models.CharField(max_length=255,null=False, unique=True)
+class Course(models.Model): 
+    subject = models.CharField(max_length=255, null= False)
+    description = models.TextField(null = True, blank=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)   
+    active = models.BooleanField(default=True)
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
--- e_courses/settings.py
+++ e_courses/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'e_courses.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'course_db',
+        'NAME': 'ecoursesdb',
         'USER': 'root',
         'PASSWORD': 'mh370qz8501', 
         'HOST': '',         
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'courses.User'
+
+
+
+
